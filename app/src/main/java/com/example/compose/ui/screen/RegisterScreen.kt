b6222f9559090929a7d119342e16e84c7355b908
freeze2222 <opop1094@gmail.com>
2022-09-15 22:53:01 +0300

    editTextStart

--- app/src/main/java/com/example/compose/ui/screen/RegisterScreen.kt
+++ app/src/main/java/com/example/compose/ui/screen/RegisterScreen.kt
@@ -1,21 +1,40 @@
 package com.example.compose.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun RegisterScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Text(text = "Test")
     }
 }
+
 @Preview
 @Composable
 fun RegisterScreenPreview() {
     RegisterScreen()
+}
+
+@Composable
+fun editText(hint : String) {
+    Column(
+        modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        var text by remember {
+            mutableStateOf("")
+        }
+        TextField(value = "", onValueChange = {
+            text = it
+        }, label = {Text(text = hint)})
+    }
 }
